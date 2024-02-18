f52b5961ef8b17f4de4abd989a440fb222cb4cbc
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-02-18 09:09:15 -0800

    0x1B. C - Sorting algorithms & Big O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,48 @@
 #include "sort.h"
-/**
- * insertion_sorted_list - function that sorted in a doubly linked list
- * integers in ascend to order to using the Insertion sorted in algorithm
- * @list: Double linked list to sorted
- */
-void insertion_sort_list(listint_t **list)
-{
-	listint_t *node;
 
-	if (list == NULL || (*list)->next == NULL)
-		return;
-	node = (*list)->next;
-	while (node)
-	{
-		while ((node->previous) && (node->previous->n > node->n))
-		{
-			node = swap_previous_node(node, list);
-			print_list(*list);
-		}
-		node = node->next;
-	}
-}
 /**
- *swap_previous_node - swap node for his previous one
- *@N: current node
- *@L: node lists
- *Return: return pointers to a node which enter it
+ * swap_node - Swap node in a listint_t to doubly-linked list.
+ * @h: pointer to head of doubly-linked list.
+ * @n1: pointer to first node to swap.
+ * @n2: second node to swap.
  */
-listint_t *swap_previous_node(listint_t *N, listint_t **L)
+void swap_node(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	listint_t *back = node->previous, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
-
-	back->next = current->next;
-	if (current->next)
-		current->next->previous = back;
-	current->next = back;
-	current->previous = back->previous;
-	back->previous = current;
-	if (current->previous)
-		current->previous->next = current;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
 	else
-		*list = current;
-	return (current);
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insert_sort_list - Sort a doubly linked list to integer
+ *                       using the insert sort algorithm.
+ * @list: pointer to head of doubly-linked list of integer.
+ *
+ * Description: Print the list after each swap.
+ */
+void insert_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_node(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
